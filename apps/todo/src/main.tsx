57d8b8f018860680322d8627e44195cc42692c8b
William Ghelfi <william.ghelfi@covisian.com>
2019-11-18 14:09:51 +0100

    feat: persist redux store in localStorage
    Quite useless at the moment, will come in handy in the future
    
    closes #31

--- apps/todo/src/main.tsx
+++ apps/todo/src/main.tsx
@@ -6,9 +6,10 @@ import ReactDOM from 'react-dom';
 // Third Parties
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // Redux
-import { store } from './app/redux/store';
+import { store, persistor } from './app/redux/store';
 
 // App Components
 import { App } from './app/app.component';
@@ -16,9 +17,11 @@ import { App } from './app/app.component';
 const render = () =>
   ReactDOM.render(
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>,
     document.getElementById('root')
   );
